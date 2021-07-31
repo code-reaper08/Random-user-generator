56be3bceb2c91e2ef9499550a83c3a0270733f45
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-31 23:32:59 +0530

    Added fetching

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            firstName: 'Revathi',
-            lastName: 'R',
-            email: 'rev1234@gmail.com',
-            gender: 'male',
-            picture: 'https://randomuser.me/api/portraits/women/13.jpg'
+            firstName: "Click the button to generate random users",
+            lastName: null,
+            email: null,
+            gender: null,
+            picture: null
+        }
+    },
+    methods: {
+        async getData() {
+            const res = await fetch('https://randomuser.me/api');
+            const {results} = await res.json();
+            console.log("fetching......");
+            this.firstName =  results[0].name.first;
+            this.lastName = results[0].name.last;
+            this.email = results[0].email;
+            this.gender = results[0].gender;
+            this.picture = results[0].picture.large;
         }
     }
 })
