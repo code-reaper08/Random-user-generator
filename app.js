d29c120e59d17578cea461000f800a4f155d574e
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-31 22:27:23 +0530

    Added CSS stylings

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const app = Vue.createApp({
-    template: '<h1>Hello world</h1>',
+    data() {
+        return {
+            firstName: 'Revathi',
+            lastName: 'R',
+            email: 'rev1234@gmail.com',
+            gender: 'female',
+            picture: 'https://randomuser.me/api/portraits/women/11.jpg'
+        }
+    }
 })
 
 app.mount('#App')
